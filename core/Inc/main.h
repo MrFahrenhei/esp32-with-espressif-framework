0cd1d1109082589f6aa463978fd3b52d648f3d45
Vinícius Valle Beraldo <vvberaldo@proton.me>
2026-05-28 14:49:30 -0300

    todo o projeto

---
+++ core/Inc/main.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#define pdSECOND pdMS_TO_TICKS(1000)
+#include "Gpio.hpp"
+class Main final
+{
+    public:
+        esp_err_t setup(void);
+        void loop(void);
+        Gpio::GpioOutput led{GPIO_NUM_25, true};
+};
---
+++ core/Src/main.cpp
@@ -0,0 +1,38 @@
+#include "main.h"
+#include "esp_event.h"
+#define LOG_LEVEL_LOCAL ESP_LOG_VERBOSE
+#include "esp_log.h"
+#define LOG_TAG "MAIN"
+
+static Main my_main;
+
+extern "C" void app_main(void)
+{
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    ESP_ERROR_CHECK(my_main.setup());
+    while(true)
+    {
+        my_main.loop();
+    }
+}
+
+esp_err_t Main::setup(void)
+{
+    esp_err_t status{ESP_OK};
+    ESP_LOGI(LOG_TAG, "Setup!");
+    status |= led.init();
+    return status;
+}
+
+void Main::loop(void)
+{
+    ESP_LOGI(LOG_TAG, "Hello World");
+    ESP_LOGI(LOG_TAG, "Led on");
+    led.set(true);
+    vTaskDelay(pdSECOND);
+    ESP_LOGI(LOG_TAG, "Led off");
+    led.set(false);
+    vTaskDelay(pdSECOND);
+
+}
